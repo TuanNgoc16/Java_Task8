fa579b2501bdf58f722bed7f339c71de9ac43988
Tuấn Ngọc <103101440+TuanNgoc16@users.noreply.github.com>
2022-04-25 15:01:35 +0700

    Update ClassRoom.java

--- src/ClassRoom.java
+++ src/ClassRoom.java
@@ -1,16 +1,24 @@
 public class ClassRoom {
-    int ClassID;
-    String ClassName;
-    public ClassRoom (){
-        ClassID = 21;
-        ClassName = " T2109A";
 
+        int ClassID;
+        String ClassName;
+        public ClassRoom (){
+            ClassID = 21;
+            ClassName = " T2109A";
+
+        }
+        public ClassRoom (int id ,  String name){
+            ClassID = id;
+            ClassName = name;
+        }
+        public String toString () {
+            return "ClassID" + ":" + ClassID + "," + "ClassName" + ":" + ClassName;}
+
+            public static void main (String[]args){
+            ClassRoom b = new ClassRoom( 1, "AD2109a");
+            System.out.println(b);
+
+
+        }
     }
-    public ClassRoom (int id ,  String name){
-        ClassID = id;
-        ClassName = name;
-    }
-    public String toString () {
-        return "ClassID" + ":" + ClassID + ";" + "ClassName" + ":" + ClassName;
-    }
-}
+
