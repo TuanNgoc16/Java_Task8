7d320f2fec5b9895c8cd67614e2750e61b241188
Tuấn Ngọc <103101440+TuanNgoc16@users.noreply.github.com>
2022-04-25 15:00:21 +0700

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,15 +1,24 @@
 public class Student {
-    int RollNo;
-    String name;
-    ClassRoom myClass;
-    public Student (int RollNo, String name){
-        this.name = name;
-        this.RollNo= RollNo;
-    }
-    public Student ( int RollNo, String name, ClassRoom myClass){
-        this.RollNo=RollNo;
-        this.name= name;
-        myClass = new ClassRoom();
 
+        int RollNo;
+        String name;
+        ClassRoom myClass;
+        public Student (int RollNo, String name){
+            this.name = name;
+            this.RollNo= RollNo;
+        }
+        public Student ( int RollNo, String name, ClassRoom myClass){
+            this.RollNo=RollNo;
+            this.name= name;
+            this.myClass = new ClassRoom();
+
+        }
+        public String toString (){
+            return "RollNo " + RollNo + ";" + "Name " + name + "; MyClass: " +  myClass;
+
+        }
+         public static void main(String[] args) {
+            Student a = new Student(1, "Tuan ngoc",new ClassRoom(1,"t12"));
+            System.out.println(a);
+        }
     }
-}
